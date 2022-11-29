5e9c4bb1c9000fcb558f97200853a365444fa15e
caduxl007 <eduardo2020dev@gmail.com>
2022-11-28 23:33:18 -0300

    test: ensure RemoteAuthentication throws UnexpectedError on default cases

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -4,6 +4,7 @@ import { mockAuthentication } from '../../../domain/test/mock-authentication'
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
 import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import faker from 'faker'
+import { UnexpectedError } from '@/domain/errors/unexpected-error copy'
 
 type SutTypes = {
   sut: RemoteAuthentication
@@ -51,5 +52,44 @@ describe('RemoteAuthentication', () => {
     const promise = sut.auth(authenticationParams)
 
     await expect(promise).rejects.toThrow(new InvalidCredentialsError())
+  })
+
+  test('Should throw InvalidCredentialsError if HttpPostClient returns 400', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.badRequest
+    }
+
+    const authenticationParams = mockAuthentication()
+
+    const promise = sut.auth(authenticationParams)
+
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
+  test('Should throw InvalidCredentialsError if HttpPostClient returns 404', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.notFound
+    }
+
+    const authenticationParams = mockAuthentication()
+
+    const promise = sut.auth(authenticationParams)
+
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
+  test('Should throw InvalidCredentialsError if HttpPostClient returns 500', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.serverError
+    }
+
+    const authenticationParams = mockAuthentication()
+
+    const promise = sut.auth(authenticationParams)
+
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
